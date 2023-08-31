b613471fd75fa101c0522b9eb81c3c38f5539ae4
PavanDivi <divipavankumar11@gmail.com>
2023-08-31 11:01:15 +0530

    date variable

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,6 +3,10 @@
 #shell script run the command inside $() and stores the output into variable called DATE.
 DATE=$(date)
 HOSTNAME=$(hostname)
+WHOAMI=$(whoami)
+WHO=$(who)
 
 echo "Hello, this script is executed at timestamp: $DATE"
 echo "This is for testing: $HOSTNAME"
+echo "Current Running UserName: $WHOAMI"
+echo "How many members has logged into the server: $WHO"
