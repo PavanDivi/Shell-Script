291ea5b3f1457dfb73cd6e77d11a2a66a56cf43e
PavanDivi <divipavankumar11@gmail.com>
2023-09-01 11:16:10 +0530

    Variable example 4 - Addition of 2 numbers

---
+++ 04-variables.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+echo "Im learning Shell-Script and Git"
+
+#Additon of two numbers
+
+Number1=$1
+Number2=$2
+
+SUM=$((Number1+Number2))
+
+echo "The addition of two numbers is: $SUM"
+
+
