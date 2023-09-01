499a0bbf5f85ee4de69d1dfbf675b10b6c07fe16
PavanDivi <divipavankumar11@gmail.com>
2023-09-01 11:07:54 +0530

    Variables example 3

---
+++ 03-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Im learning Shellscripting and Git"
+
+Person1=$1
+Person2=$2
+
+echo "$person1: Hi $Person2, Good Morning"
+echo "$person2: hi $person1, Very Ggood Morning"
+echo "$person1: How are you $Persona2"
+echo "$person2: Im fine $Person1, what about you?"
+echo "$person1: Im good $person2"
