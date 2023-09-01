43defc3e556dafefcf446f11236bcc843d5291ea
PavanDivi <divipavankumar11@gmail.com>
2023-09-01 11:37:37 +0530

    Variables example 3

--- 03-variables.sh
+++ 03-variables.sh
@@ -6,7 +6,7 @@ Person1=$1
 Person2=$2
 
 echo "$Person1: Hi $Person2, Good Morning"
-echo "$Person2: hi $Person1, Very Ggood Morning"
+echo "$Person2: hi $Person1, Very Good Morning"
 echo "$Person1: How are you $Person2"
 echo "$Person2: Im fine $Person1, what about you?"
 echo "$Person1: Im good $Person2"
